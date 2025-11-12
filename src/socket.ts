ee72a385ee4c96d21dfb80b3dd08ff48562d3744
tobias-fernandes <tobys312@gmail.com>
2025-11-11 21:08:14 -0300

    test

--- src/socket.ts
+++ src/socket.ts
@@ -1,11 +1,8 @@
 import { io } from "socket.io-client";
 
 // Configuração do socket.io client com o endereço do servidor
-const socket = io(
-  "wss://aqua-sheriff-continues-secretariat.trycloudflare.com",
-  {
-    transports: ["websocket"],
-  }
-);
+const socket = io("wss://watches-concrete-passes-gore.trycloudflare.com", {
+  transports: ["websocket"],
+});
 
 export { socket };
